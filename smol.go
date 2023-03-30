5f579e7e4429400c9def985ce82cefad3fdcfe74
ndfsa <25588077+ndfsa@users.noreply.github.com>
2023-03-29 23:47:31 -0700

    fully functional

--- smol.go
+++ smol.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 func main() {
@@ -18,37 +19,59 @@ func main() {
 
 	for _, file_name := range args {
 		if *extract_mode {
-			log.Printf("decompressing %s", file_name)
+			decompress(file_name)
 		} else {
 			compress(file_name)
 		}
 	}
 }
 
+func check(err error) {
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+}
+
 func compress(file_name string) {
 	abs_path, err := filepath.Abs(file_name)
-	if err != nil {
-		log.Fatalf("failed finding file with name: %s", file_name)
-	}
+	check(err)
 
 	file, err := os.Open(abs_path)
-	if err != nil {
-		log.Fatalf("failed opening file %s", abs_path)
-	}
+	defer file.Close()
+	check(err)
 
 	data, err := ioutil.ReadAll(bufio.NewReader(file))
-	if err != nil {
-		log.Fatalf("failed reading file %s", abs_path)
-	}
+	check(err)
 
 	abs_path += ".gz"
 	compressed_file, err := os.Create(abs_path)
-	if err != nil {
-		log.Fatalf("failed creating compressed file %s", abs_path)
-	}
+	defer compressed_file.Close()
+	check(err)
 
 	compressed_writer := gzip.NewWriter(compressed_file)
 	compressed_writer.Write(data)
 	compressed_writer.Close()
+}
 
+func decompress(file_name string) {
+	abs_path, err := filepath.Abs(file_name)
+	check(err)
+
+	file, err := os.Open(abs_path)
+	defer file.Close()
+	check(err)
+
+	reader, err := gzip.NewReader(file)
+	check(err)
+	data, err := ioutil.ReadAll(reader)
+	check(err)
+
+	abs_path = strings.TrimSuffix(abs_path, ".gz")
+	decompressed_file, err := os.Create(abs_path)
+	defer decompressed_file.Close()
+	check(err)
+
+	writer := bufio.NewWriter(decompressed_file)
+	writer.Write(data)
+	writer.Flush()
 }
